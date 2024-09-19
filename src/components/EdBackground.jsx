33bc2d9568cea705b3da7af70d87ce1ec8f4a5d5
Diogo_Oike <diogooikekanefukujapan@gmail.com>
2024-09-19 10:16:48 -0300

    EB hover instead click

--- src/components/EdBackground.jsx
+++ src/components/EdBackground.jsx
@@ -4,27 +4,20 @@ import './EdBackground.css'
 const Skills = () => {
   const [activeSection, setActiveSection] = useState('EBCertifies')
 
-  const switchClass = (btnClicked, btnChange, colClicked, colChange) => {
-    setActiveSection(btnClicked)
+  const handleMouseEnter = (mouseIn) => {
+    setActiveSection(mouseIn)
   }
 
   return (
     <section id='EB' className='sectionMain'>
       <h2 className='titleSection'>ACADEMIC BACKGROUND</h2>
-      <div className="EBContent">
+      <div className='EBContent'>
         <div className='EBOptions'>
           <div
             className={`EBCertifies ${
               activeSection === 'EBCertifies' ? 'EBActive' : 'EBOff'
             }`}
-            onClick={() =>
-              switchClass(
-                'EBCertifies',
-                'EBAcademic',
-                'EBCertifiesCol',
-                'EBAcademicCol'
-              )
-            }
+            onMouseEnter={() => handleMouseEnter('EBCertifies')}
           >
             <h3>Courses/certificates</h3>
           </div>
@@ -32,14 +25,7 @@ const Skills = () => {
             className={`EBAcademic ${
               activeSection === 'EBAcademic' ? 'EBActive' : 'EBOff'
             }`}
-            onClick={() =>
-              switchClass(
-                'EBAcademic',
-                'EBCertifies',
-                'EBAcademicCol',
-                'EBCertifiesCol'
-              )
-            }
+            onMouseEnter={() => handleMouseEnter('EBAcademic')}
           >
             <h3>Academic</h3>
           </div>
@@ -49,14 +35,7 @@ const Skills = () => {
             className={`EBCertifiesCol ${
               activeSection === 'EBCertifies' ? 'EBColActive' : 'EBColOff'
             }`}
-            onClick={() =>
-              switchClass(
-                'EBCertifies',
-                'EBAcademic',
-                'EBCertifiesCol',
-                'EBAcademicCol'
-              )
-            }
+            onMouseEnter={() => handleMouseEnter('EBCertifies')}
           >
             <div className='EBCertifiesText'>
               <ul className='EBCertifiesList'>
@@ -73,14 +52,7 @@ const Skills = () => {
             className={`EBAcademicCol ${
               activeSection === 'EBAcademic' ? 'EBColActive' : 'EBColOff'
             }`}
-            onClick={() =>
-              switchClass(
-                'EBAcademic',
-                'EBCertifies',
-                'EBAcademicCol',
-                'EBCertifiesCol'
-              )
-            }
+            onMouseEnter={() => handleMouseEnter('EBAcademic')}
           >
             <p className='textDescription'>
               Information Technology Management (GTI) student at IFSC in
@@ -88,9 +60,10 @@ const Skills = () => {
               logic using the language{' '}
               <span className='styleTech styleCPP'>C++</span> and object
               orientation using <span className='styleTech styleJV'>Java</span>.
-              Furthermore, I learned about network and management basics. This in
-              disciplines such as: Network infrastructure, systems development
-              processes, people management and economic and financial management.
+              Furthermore, I learned about network and management basics. This
+              in disciplines such as: Network infrastructure, systems
+              development processes, people management and economic and
+              financial management.
             </p>
           </div>
         </div>
