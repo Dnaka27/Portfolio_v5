04117091ef08501a5e0ee5b99b837b5a9b0e5e8d
Diogo_Oike <diogooikekanefukujapan@gmail.com>
2024-09-21 03:13:31 -0300

    Update project hover

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,40 @@
-import './Projects.css'
+import { useState } from 'react';
+import './Projects.css';
 
 const Projects = () => {
+  const [description, setDescription] = useState('text');
+
+  const handleMouseEnter = () => {
+    setDescription('Novo texto ao passar o mouse!');
+  };
+
+  const handleMouseLeave = () => {
+    setDescription('<i class="fa-brands fa-python project-tech project-py"></i>'); // Texto original com HTML
+  };
+
   return (
-    <section id='projects' class='sectionMain'>
+    <section id='projects' className='sectionMain'>
       <h2 className='titleSection'>PROJECTS</h2>
-      <div id='carousel' class='carousel slide'>
-        <div class='carousel-inner'>
+      <div id='carousel' className='carousel slide'>
+        <div className='carousel-inner'>
           <div className='carousel-item active'>
-            <div className='carousel-schema-primary'>
+            <div
+              className='carousel-schema-primary'
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
               <div className='carousel-image'>
-                <a className='carousel-image-project'></a>
+                <a
+                  href='https://coinviewer-diogooike.streamlit.app/'
+                  className='carousel-image-project image-project1'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  alt='Project image'
+                ></a>
               </div>
               <div className='carousel-desc'>
-                <h4>Project Name</h4>
-                <p>text</p>
+                <h4>Coin Viewer</h4>
+                <p dangerouslySetInnerHTML={{ __html: description }} /> {/* Aqui */}
               </div>
             </div>
             <div className='carousel-schema-primary'>
@@ -22,185 +43,33 @@ const Projects = () => {
                 <p>text</p>
               </div>
               <div className='carousel-image'>
-                <a href=''></a>
+                <a className='carousel-image-project' href=''></a>
               </div>
             </div>
           </div>
           <div className='carousel-item'></div>
-          {/* <div class='carousel-item'>
-            <div class='carouselProjects'>
-              <div class='carouselImageCol'>
-                <a
-                  class='carouselProjectImage projectImage1'
-                  href='https://dnaka27.github.io/Tindog_Udemy_Web/'
-                  target='_blank'
-                  alt='Project image'
-                ></a>
-              </div>
-              <div class='carouselDescriptionCol'>
-                <h3 class='titleProjectDescription'>TinDoG</h3>
-                <p class='textDescription'>
-                  This project was completed as part of the Fullstack
-                  Programming course on Udemy. It’s a dating app for dogs called{' '}
-                  <a
-                    class='projectName'
-                    href='https://github.com/Dnaka27/Tindog_Udemy_Web'
-                    target='_blank'
-                  >
-                    TINDOG
-                  </a>
-                  . I used <span class='styleTech styleHTML'>HTML</span> and{' '}
-                  <span class='styleTech styleCSS'>CSS</span> to create a static
-                  page layout, also utilizing{' '}
-                  <span class='styleTool'>BOOTSTRAP</span> for responsiveness
-                  and components.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class='carousel-item'>
-            <div class='carouselProjects'>
-              <div class='carouselImageCol'>
-                <a
-                  class='carouselProjectImage projectImage2'
-                  href='https://dnaka27.github.io/ToDoList_basic/'
-                  target='_blank'
-                  alt='Project image'
-                ></a>
-              </div>
-              <div class='carouselDescriptionCol'>
-                <h3 class='titleProjectDescription'>To do list</h3>
-                <p class='textDescription'>
-                  This is an organizational project, a simple to-do list for
-                  everyday tasks, but with filters and a search bar. It's called{' '}
-                  <a
-                    class='projectName'
-                    href='https://github.com/Dnaka27/ToDoList_basic/'
-                    target='_blank'
-                  >
-                    TO DO LIST
-                  </a>
-                  . In this project, I used{' '}
-                  <span class='styleTech styleJS'>JAVASCRIPT</span> along with{' '}
-                  <span class='styleTech styleReact'>REACT</span> and finally
-                  styled it with <span class='styleTech styleCSS'>CSS</span>.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class='carousel-item active'>
-            <div class='carouselProjects'>
-              <div class='carouselImageCol'>
-                <a
-                  class='carouselProjectImage projectImage3'
-                  href='https://coinviewer-diogooike.streamlit.app/'
-                  target='_blank'
-                  alt='Project image'
-                ></a>
-              </div>
-              <div class='carouselDescriptionCol'>
-                <h3 class='titleProjectDescription'>Coin Viewer</h3>
-                <p class='textDescription'>
-                  This is a personal project that is still in progress, a
-                  currency quotation application called{' '}
-                  <a
-                    class='projectName'
-                    href='https://github.com/Dnaka27/Coin_Viewer'
-                    target='_blank'
-                  >
-                    COIN VIEWER
-                  </a>
-                  . It is primarily built with{' '}
-                  <span class='styleTech stylePY'>PYTHON</span>, using{' '}
-                  <span class='styleTool'>STREAMLIT</span> for the interface,{' '}
-                  <span class='styleTool'>PANDAS</span> and{' '}
-                  <span class='styleTool'>NUMPY</span> for data handling,{' '}
-                  <span class='styleTool'>REQUESTS</span> for the API, and{' '}
-                  <span class='styleTool'>PLOTLY</span> for the charts.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class='carousel-item'>
-            <div class='carouselProjects'>
-              <div class='carouselImageCol'>
-                <a
-                  class='carouselProjectImage projectImage4'
-                  href='https://github.com/Dnaka27/Basic_system_JavaSpring'
-                  target='_blank'
-                  alt='Project image'
-                ></a>
-              </div>
-              <div class='carouselDescriptionCol'>
-                <h3 class='titleProjectDescription'>Java API</h3>
-                <p class='textDescription'>
-                  This is a basic project made with{' '}
-                  <span class='styleTech styleJV'>Java</span>, called{' '}
-                  <a
-                    href='https://github.com/Dnaka27/Basic_system_JavaSpring'
-                    class='projectName'
-                  >
-                    JAVA API
-                  </a>
-                  . It’s a simple system integrated with an H2 database. I used
-                  the <span class='styleTool'>SPRING</span> framework and{' '}
-                  <span class='styleTool'>MAVEN</span> to manage its
-                  dependencies.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class='carousel-item'> */}
-          {/* <div class='carouselProjects'>
-              <div class='carouselImageCol'>
-                <a
-                  class='carouselProjectImage projectImage5'
-                  href='https://github.com/Dnaka27/Nutritionix_API-Google_sheets'
-                  target='_blank'
-                  alt='Project image'
-                ></a>
-              </div>
-              <div class='carouselDescriptionCol'>
-                <h3 class='titleProjectDescription'>Nutritionix project</h3>
-                <p class='textDescription'>
-                  A project completed as part of the Python programming course
-                  on Udemy, called{' '}
-                  <a
-                    class='projectName'
-                    href='https://github.com/Dnaka27/Nutritionix_API-Google_sheets'
-                    target='_blank'
-                  >
-                    NUTRITIONIX
-                  </a>
-                  . It involves integrating a{' '}
-                  <span class='styleTool'>SHEETS</span> spreadsheet with an API
-                  call using <span class='styleTech stylePY'>PYTHON</span> and
-                  the <span class='styleTool'>SHEETY</span> tool.
-                </p>
-              </div>
-            </div> */}
         </div>
         <button
-          class='carousel-control-prev'
+          className='carousel-control-prev'
           type='button'
           data-bs-target='#carousel'
           data-bs-slide='prev'
         >
-          <span class='carousel-control-prev-icon' aria-hidden='true'></span>
-          <span class='visually-hidden'>Previous</span>
+          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+          <span className='visually-hidden'>Previous</span>
         </button>
         <button
-          class='carousel-control-next'
+          className='carousel-control-next'
           type='button'
           data-bs-target='#carousel'
           data-bs-slide='next'
         >
-          <span class='carousel-control-next-icon' aria-hidden='true'></span>
-          <span class='visually-hidden'>Next</span>
+          <span className='carousel-control-next-icon' aria-hidden='true'></span>
+          <span className='visually-hidden'>Next</span>
         </button>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
