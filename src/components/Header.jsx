bcd8fe306e198b1b9e5ebf50bf09c9e739e9f473
Diogo_Oike <diogooikekanefukujapan@gmail.com>
2024-10-26 23:06:14 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,7 +64,7 @@ const Header = () => {
       <div className='my-info'>
         <div className='my-role'>
           <h1>&lt;Diogo Oike/&gt;</h1>
-          <div className="my-functions">
+          <div className='my-functions'>
             <h3>DATA ENGINEER</h3>
             <h3>FULLSTACK DEVELOPER</h3>
           </div>
@@ -72,18 +72,23 @@ const Header = () => {
         <div className='about-me'>
           <h2>About me</h2>
           <p className='about-text'>
-            Nice to meet you, I'm Diogo! I'm a university student pursuing a
-            degree in software engineering. I apply my knowledge in{' '}
+            Nice to meet you, I'm Diogo! I'm a university student focused on
+            software development and data engineering, with my knowledge in{' '}
             <span className='styleTech stylePY'>Python</span>,{' '}
             <span className='styleTech styleJV'>Java</span>, and{' '}
-            <span className='styleTech styleJS'>JavaScript</span> in the field.
-            I enjoy creating smart solutions while exploring the world of
-            technology. I'm a committed and creative person, always looking to{' '}
-            <span className='styleEnph'>learn</span> something new.
+            <span className='styleTech styleJS'>JavaScript</span>. I enjoy
+            creating smart solutions while exploring the world of technology. I
+            am a committed and creative person, always looking to{' '}
+            <span className='styleEnph'>learn </span>
+            something new.
           </p>
         </div>
       </div>
-      <button className='fixed-button' onClick={() => scrollToSection('menu')} title='Back to menu'>
+      <button
+        className='fixed-button'
+        onClick={() => scrollToSection('menu')}
+        title='Back to menu'
+      >
         <i className='fa-solid fa-arrow-up'></i>
       </button>
     </header>
