8e5668d69dfa80a2cd0197fad915b3ace33134af
Diogo_Oike <diogooikekanefukujapan@gmail.com>
2024-10-29 00:28:25 -0300

    test carousel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import './Projects.css';
+import './Projects.css'
 
 // Dados dos projetos
 const projectData = [
@@ -14,7 +14,8 @@ const projectData = [
           rel='noopener noreferrer'
         >
           COIN VIEWER
-        </a>. It is primarily built with{' '}
+        </a>
+        . It is primarily built with{' '}
         <span className='styleTech stylePY'>PYTHON</span>, using{' '}
         <span className='styleTool'>STREAMLIT</span> for the interface,{' '}
         <span className='styleTool'>PANDAS</span> and{' '}
@@ -39,16 +40,17 @@ const projectData = [
           rel='noopener noreferrer'
         >
           TO DO LIST
-        </a>. In this project, I used{' '}
+        </a>
+        . In this project, I used{' '}
         <span className='styleTech styleJS'>JAVASCRIPT</span> along with{' '}
-        <span className='styleTech styleReact'>REACT</span> and finally styled it
-        with <span className='styleTech styleCSS'>CSS</span>.
+        <span className='styleTech styleReact'>REACT</span> and finally styled
+        it with <span className='styleTech styleCSS'>CSS</span>.
       </>
     ),
     imageUrl: 'https://dnaka27.github.io/ToDoList_basic/',
     githubRepository: 'https://github.com/Dnaka27/ToDoList_basic/',
   },
-];
+]
 
 const Projects = () => {
   return (
@@ -57,11 +59,11 @@ const Projects = () => {
       <div id='carousel' className='carousel slide'>
         <div className='carousel-inner'>
           <div className='carousel-item active'>
-            <div className='carousel-schema-primary'>
-              <div className='carousel-image-primary'>
+            <div className='schema-primary'>
+              <div className='image-primary'>
                 <a
                   href={projectData[0].imageUrl}
-                  className='carousel-image-project image-project1'
+                  className='image-project image-project1'
                   target='_blank'
                   rel='noopener noreferrer'
                   aria-label='Coin Viewer Project'
@@ -69,7 +71,9 @@ const Projects = () => {
               </div>
               <div className='carousel-desc-primary'>
                 <h4 className='carousel-desc-title'>{projectData[0].name}</h4>
-                <p className='carousel-desc-text'>{projectData[0].description}</p>
+                <p className='carousel-desc-text'>
+                  {projectData[0].description}
+                </p>
                 <div className='carousel-desc-primary-links'>
                   <a
                     href={projectData[0].githubRepository}
@@ -82,11 +86,11 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-            <div className='carousel-schema-primary'>
-              <div className='carousel-image-primary'>
+            <div className='schema-primary'>
+              <div className='image-primary'>
                 <a
                   href={projectData[1].imageUrl}
-                  className='carousel-image-project image-project2'
+                  className='image-project image-project2'
                   target='_blank'
                   rel='noopener noreferrer'
                   aria-label='To Do List Project'
@@ -94,7 +98,9 @@ const Projects = () => {
               </div>
               <div className='carousel-desc-primary'>
                 <h4 className='carousel-desc-title'>{projectData[1].name}</h4>
-                <p className='carousel-desc-text'>{projectData[1].description}</p>
+                <p className='carousel-desc-text'>
+                  {projectData[1].description}
+                </p>
                 <div className='carousel-desc-primary-links'>
                   <a
                     href={projectData[1].githubRepository}
@@ -108,7 +114,35 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          
+          <div className='carousel-item'>
+          <div className='schema-primary'>
+              <div className='image-primary'>
+                <a
+                  href={projectData[0].imageUrl}
+                  className='image-project image-project1'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  aria-label='Coin Viewer Project'
+                ></a>
+              </div>
+              <div className='carousel-desc-primary'>
+                <h4 className='carousel-desc-title'>{projectData[0].name}</h4>
+                <p className='carousel-desc-text'>
+                  {projectData[0].description}
+                </p>
+                <div className='carousel-desc-primary-links'>
+                  <a
+                    href={projectData[0].githubRepository}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='GitHub Repository'
+                  >
+                    <i className='fa-brands fa-github carousel-desc-links-icon'></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <button
           className='carousel-control-prev'
@@ -117,7 +151,10 @@ const Projects = () => {
           data-bs-slide='prev'
           aria-label='Previous slide'
         >
-          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+          <span
+            className='carousel-control-prev-icon'
+            aria-hidden='true'
+          ></span>
           <span className='visually-hidden'>Previous</span>
         </button>
         <button
@@ -127,12 +164,15 @@ const Projects = () => {
           data-bs-slide='next'
           aria-label='Next slide'
         >
-          <span className='carousel-control-next-icon' aria-hidden='true'></span>
+          <span
+            className='carousel-control-next-icon'
+            aria-hidden='true'
+          ></span>
           <span className='visually-hidden'>Next</span>
         </button>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
