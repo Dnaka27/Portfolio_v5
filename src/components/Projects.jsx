69cfcee3265efd4213356afd65549aef4045a296
Diogo_Oike <diogooikekanefukujapan@gmail.com>
2024-11-01 23:41:08 -0300

    Projects layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const projectData = [
         <span className='styleTool'>PLOTLY</span> for the charts.
       </>
     ),
-    imageUrl: 'https://coinviewer-diogooike.streamlit.app/',
+    imageUrl: 'images/Tindog.png',
     githubRepository: 'https://github.com/Dnaka27/Coin_Viewer',
   },
   {
@@ -43,106 +43,71 @@ const projectData = [
         </a>
         . In this project, I used{' '}
         <span className='styleTech styleJS'>JAVASCRIPT</span> along with{' '}
-        <span className='styleTech styleReact'>REACT</span> and finally styled
+        <span className='styleTech styleReact'>REACT</span> and styled
         it with <span className='styleTech styleCSS'>CSS</span>.
       </>
     ),
     imageUrl: 'https://dnaka27.github.io/ToDoList_basic/',
     githubRepository: 'https://github.com/Dnaka27/ToDoList_basic/',
   },
+  // Outros projetos podem ser adicionados aqui.
 ]
 
+// Componente para exibir um projeto individual
+const ProjectItem = ({ project, index }) => (
+  <div className='schema-primary'>
+    <div className={`image-primary ${index === 0 ? 'image-project1' : 'image-project2'}`}>
+      <a
+        href={project.imageUrl}
+        className='image-project'
+        target='_blank'
+        rel='noopener noreferrer'
+        aria-label={`${project.name} Project`}
+      >
+        {/* A tag <img> não é mais necessária, pois estamos usando o background */}
+      </a>
+    </div>
+    <div className='carousel-desc-primary'>
+      <h4 className='carousel-desc-title'>{project.name}</h4>
+      <p className='carousel-desc-text'>{project.description}</p>
+      <div className='carousel-desc-primary-links'>
+        <a
+          href={project.githubRepository}
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='GitHub Repository'
+        >
+          <i className='fa-brands fa-github carousel-desc-links-icon'></i>
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+
+
+// Componente principal para o carrossel de projetos
 const Projects = () => {
+  // Agrupa projetos em pares
+  const projectPairs = [];
+  for (let i = 0; i < projectData.length; i += 2) {
+    projectPairs.push(projectData.slice(i, i + 2));
+  }
+
   return (
     <section id='projects' className='sectionMain'>
       <h2 className='titleSection'>PROJECTS</h2>
       <div id='carousel' className='carousel slide'>
         <div className='carousel-inner'>
-          <div className='carousel-item active'>
-            <div className='schema-primary'>
-              <div className='image-primary'>
-                <a
-                  href={projectData[0].imageUrl}
-                  className='image-project image-project1'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  aria-label='Coin Viewer Project'
-                ></a>
-              </div>
-              <div className='carousel-desc-primary'>
-                <h4 className='carousel-desc-title'>{projectData[0].name}</h4>
-                <p className='carousel-desc-text'>
-                  {projectData[0].description}
-                </p>
-                <div className='carousel-desc-primary-links'>
-                  <a
-                    href={projectData[0].githubRepository}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    aria-label='GitHub Repository'
-                  >
-                    <i className='fa-brands fa-github carousel-desc-links-icon'></i>
-                  </a>
-                </div>
+          {projectPairs.map((pair, index) => (
+            <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+              <div className='project-pair'>
+                {pair.map((project, idx) => (
+                  <ProjectItem key={idx} project={project} index={idx} />
+                ))}
               </div>
             </div>
-            <div className='schema-primary'>
-              <div className='image-primary'>
-                <a
-                  href={projectData[1].imageUrl}
-                  className='image-project image-project2'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  aria-label='To Do List Project'
-                ></a>
-              </div>
-              <div className='carousel-desc-primary'>
-                <h4 className='carousel-desc-title'>{projectData[1].name}</h4>
-                <p className='carousel-desc-text'>
-                  {projectData[1].description}
-                </p>
-                <div className='carousel-desc-primary-links'>
-                  <a
-                    href={projectData[1].githubRepository}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    aria-label='GitHub Repository'
-                  >
-                    <i className='fa-brands fa-github carousel-desc-links-icon'></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='carousel-item'>
-          <div className='schema-primary'>
-              <div className='image-primary'>
-                <a
-                  href={projectData[0].imageUrl}
-                  className='image-project image-project1'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  aria-label='Coin Viewer Project'
-                ></a>
-              </div>
-              <div className='carousel-desc-primary'>
-                <h4 className='carousel-desc-title'>{projectData[0].name}</h4>
-                <p className='carousel-desc-text'>
-                  {projectData[0].description}
-                </p>
-                <div className='carousel-desc-primary-links'>
-                  <a
-                    href={projectData[0].githubRepository}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    aria-label='GitHub Repository'
-                  >
-                    <i className='fa-brands fa-github carousel-desc-links-icon'></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
         <button
           className='carousel-control-prev'
@@ -151,10 +116,7 @@ const Projects = () => {
           data-bs-slide='prev'
           aria-label='Previous slide'
         >
-          <span
-            className='carousel-control-prev-icon'
-            aria-hidden='true'
-          ></span>
+          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
           <span className='visually-hidden'>Previous</span>
         </button>
         <button
@@ -164,15 +126,12 @@ const Projects = () => {
           data-bs-slide='next'
           aria-label='Next slide'
         >
-          <span
-            className='carousel-control-next-icon'
-            aria-hidden='true'
-          ></span>
+          <span className='carousel-control-next-icon' aria-hidden='true'></span>
           <span className='visually-hidden'>Next</span>
         </button>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Projects
