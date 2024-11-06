5da7b303f77efa184e6b0df1192b5ebfc91b0368
Diogo Oike Kanefuku <diogooikekanefukujapan@gmail.com>
2024-11-06 20:41:20 -0300

    Update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,3 @@
-// Projects.js
 import './Projects.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -26,19 +25,21 @@ const ProjectCard = ({ project }) => (
       className="project-image"
       style={{ backgroundImage: `url(${project.imageUrl})` }}
     ></div>
-    <h4 className="project-title">{project.name}</h4>
-    <p className="project-description">{project.description}</p>
-    <a href={project.githubRepository} target="_blank" rel="noopener noreferrer" className="project-link">
-      Ver no GitHub
-    </a>
+    <div className="project-info">
+      <h4 className="project-title">{project.name}</h4>
+      <p className="project-description">{project.description}</p>
+      <a href={project.githubRepository} target="_blank" rel="noopener noreferrer" className="project-link">
+        <i className="fa-brands fa-square-github"></i>
+      </a>
+    </div>
   </div>
 );
 
 // Componente principal de projetos
 const Projects = () => (
-  <section id="projects" className="projects-section">
-    <h2 className="title-section">PROJECTS</h2>
-    <div className="project-grid">
+  <section id="projects" className="sectionMain">
+    <h2 className="titleSection">PROJECTS</h2>
+    <div className="project-container">
       {projectData.map((project, index) => (
         <ProjectCard key={index} project={project} />
       ))}
