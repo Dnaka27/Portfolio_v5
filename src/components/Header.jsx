530b50d9ea4ad98a8c1f99ed651eb3d0175181a4
Diogo Oike Kanefuku <diogooikekanefukujapan@gmail.com>
2025-05-23 01:04:59 -0300

    setted quote

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header className='header' id='menu'>
       <nav className='navbar navbar-expand-lg navbar-dark' id='nav-style'>
-        Welcome!
+        HOME
         <button
           className='navbar-toggler'
           type='button'
@@ -31,7 +31,7 @@ const Header = () => {
                 className='nav-link'
                 onClick={() => scrollToSection('projects')}
               >
-                Projects
+                PROJECTS
               </button>
             </li>
             <li className='nav-item'>
@@ -39,7 +39,7 @@ const Header = () => {
                 className='nav-link'
                 onClick={() => scrollToSection('EB')}
               >
-                Academic
+                ACADEMIC
               </button>
             </li>
             <li className='nav-item'>
@@ -47,7 +47,7 @@ const Header = () => {
                 className='nav-link'
                 onClick={() => scrollToSection('skills')}
               >
-                Skills
+                SKILLS
               </button>
             </li>
             <li className='nav-item'>
@@ -55,7 +55,7 @@ const Header = () => {
                 className='nav-link'
                 onClick={() => scrollToSection('contact')}
               >
-                Contact
+                CONTACT
               </button>
             </li>
           </ul>
@@ -63,27 +63,32 @@ const Header = () => {
       </nav>
       <div className='my-info'>
         <div className='my-role'>
-          <h1>Diogo Oike Kanefuku</h1>
+          <h1>DIOGO OIKE</h1>
           <div className='my-functions'>
             <div className='main-role'>
-              <span>DATA ENGINEER</span> <div className="function-icons"><i className='fa-brands fa-python'></i><i className='fa-solid fa-database'></i><i class="fa-solid fa-chart-simple"></i></div>
+              <span>DATA ENGINEER</span>{' '}
+              <div className='function-icons'>
+                <i className='fa-brands fa-python'></i>
+                <i className='fa-solid fa-database'></i>
+                <i class='fa-solid fa-chart-simple'></i>
+              </div>
             </div>
             <div className='secondary-role'>
-              <span>BACKEND DEVELOPER</span> <div className="function-icons"><i class="fa-brands fa-node-js"></i><i class="fa-solid fa-bolt"></i></div>
+              <span>BACKEND DEVELOPER</span>{' '}
+              <div className='function-icons'>
+                <i class='fa-brands fa-node-js'></i>
+                <i class='fa-solid fa-bolt'></i>
+              </div>
             </div>
           </div>
         </div>
         <div className='about-me'>
-          <h2>About me</h2>
           <p className='about-text'>
-            Nice to meet you, I'm Diogo! I'm a university student focused on
-            software development and data engineering, with my knowledge in{' '}
-            <span className='styleTech stylePY'>Python</span>,{' '}
-            <span className='styleTech styleJV'>Java</span>, and{' '}
-            <span className='styleTech styleJS'>JavaScript</span>. I enjoy
-            creating smart solutions while exploring the world of technology. I
-            am a committed and creative person, always looking to{' '}
-            <span className='styleEnph'>learn</span> something new.
+            Developing smart solutions with{' '}
+            <span className='header-styleTech header-stylePY'>Python</span>,{' '}
+            <span className='header-styleTech header-styleJV'>Java</span>, and{' '}
+            <span className='header-styleTech header-styleJS'>JavaScript</span>. Tech enthusiast, always{' '}
+            <span className='styleEnph'>learning</span> and evolving.
           </p>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,50 +1,58 @@
-import './Projects.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './Projects.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // Dados dos projetos
 const projectData = [
   {
     name: 'Coin Viewer',
-    description: 'A currency quotation application built with Python and Streamlit.',
+    description:
+      'A currency quotation application built with Python and Streamlit.',
     imageUrl: 'images/Coin_Viewer.png',
     githubRepository: 'https://github.com/Dnaka27/Coin_Viewer',
   },
   {
     name: 'To Do List',
-    description: 'An organizational to-do list application using React and JavaScript.',
+    description:
+      'An organizational to-do list application using React and JavaScript.',
     imageUrl: 'images/To_Do_List.png',
     githubRepository: 'https://github.com/Dnaka27/ToDoList_basic/',
   },
   // Adicione mais projetos aqui
-];
+]
 
-// Componente para exibir um projeto individual
 const ProjectCard = ({ project }) => (
-  <div className="project-card">
+  <div className='project-card' role='article'>
     <div
-      className="project-image"
+      className='project-image'
       style={{ backgroundImage: `url(${project.imageUrl})` }}
+      aria-label={`Imagem do projeto ${project.name}`}
     ></div>
-    <div className="project-info">
-      <h4 className="project-title">{project.name}</h4>
-      <p className="project-description">{project.description}</p>
-      <a href={project.githubRepository} target="_blank" rel="noopener noreferrer" className="project-link">
-        <i className="fa-brands fa-square-github"></i>
+    <div className='project-info'>
+      <h4 className='project-title'>{project.name}</h4>
+      <p className='project-description'>{project.description}</p>
+      <a
+        href={project.githubRepository}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='project-link'
+        aria-label={`Ver projeto ${project.name} no GitHub`}
+      >
+        <i className='fa-brands fa-square-github'></i>
       </a>
     </div>
   </div>
-);
+)
 
 // Componente principal de projetos
 const Projects = () => (
-  <section id="projects" className="sectionMain">
-    <h2 className="titleSection">PROJECTS</h2>
-    <div className="project-container">
+  <section id='projects' className='sectionMain'>
+    <h2 className='titleSection'>PROJECTS</h2>
+    <div className='project-container'>
       {projectData.map((project, index) => (
         <ProjectCard key={index} project={project} />
       ))}
     </div>
   </section>
-);
+)
 
-export default Projects;
+export default Projects
